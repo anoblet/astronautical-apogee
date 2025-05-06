5bbf10dd627b1dd2f1e04d4034c406188816f028
Andrew Noblet <andrewbnoblet@gmail.com>
2025-05-06 18:23:33 -0400

    feat(menu): update menu component to use `opened` property
    Replaced `isOpen` with `opened` property for better alignment with web component standards. Added `reflect: true` to synchronize attribute and property. Updated styles to improve hover behavior and ensure consistent height. Adjusted event listeners for better interaction handling. Removed unused styles and comments for cleaner code.

--- src/components/Menu.ts
+++ src/components/Menu.ts
@@ -3,10 +3,11 @@ import { customElement, property } from "lit/decorators.js";
 
 @customElement("menu-component")
 class MenuComponent extends LitElement {
-  @property({ type: Boolean }) accessor isOpen = false;
+  @property({ reflect: true, type: Boolean }) accessor opened = false;
 
   static styles = css`
     [hidden] {
+      pointer-events: none;
       opacity: 0;
       transition: opacity 0.2s ease-in-out;
     }
@@ -19,7 +20,7 @@ class MenuComponent extends LitElement {
     :host {
       display: flex;
       align-items: center;
-      gap: 2rem;
+      height: 100%;
 
       #icon {
         display: flex;
@@ -33,36 +34,45 @@ class MenuComponent extends LitElement {
 
         display: flex;
         list-style: none;
+        height: 100%;
 
         li {
+          display: flex;
+          align-items: center;
           padding: 0.5rem 2rem;
           font-weight: 500;
 
           &:hover {
             cursor: pointer;
             /* background-color: hsla(0, 0%, 100%, 0.1); */
-            color: var(--color-rose-400);
+            color: var(--color-teal-200);
           }
 
-          &:not(:last-child) {
-            border-right: 1px solid var(--color-neutral-600);
-          }
+          /* border-right: 1px solid var(--color-neutral-600); */
         }
+      }
+    }
+
+    :host([opened]) {
+      #icon {
+        /* border-right: 1px solid var(--color-neutral-600); */
       }
     }
   `;
 
   firstUpdated() {
     this.addEventListener("click", () => {
-      this.isOpen = !this.isOpen;
+      this.opened = !this.opened;
     });
 
-    this.addEventListener("mouseenter", () => {
-      this.isOpen = true;
-    });
+    this.shadowRoot
+      ?.querySelector("#icon")
+      ?.addEventListener("mouseenter", () => {
+        this.opened = true;
+      });
 
     this.addEventListener("mouseleave", () => {
-      this.isOpen = false;
+      this.opened = false;
     });
   }
 
@@ -71,7 +81,7 @@ class MenuComponent extends LitElement {
       <div id="icon">
         <slot name="icon"></slot>
       </div>
-      <ul ?hidden=${!this.isOpen}>
+      <ul ?hidden=${!this.opened}>
         <li>Projects</li>
         <li>Services</li>
       </ul>
