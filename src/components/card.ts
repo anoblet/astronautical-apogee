cfb684c60e3867b74cad4e96ca02b123ef08495c
Andrew Noblet <andrewbnoblet@gmail.com>
2025-05-10 12:58:25 -0400

    feat: add card component and project cards to projects page
    Introduce a new card component using LitElement for displaying project details. Update the projects page to include cards for showcasing specific projects with images and descriptions. Enhance global styles and layout for better alignment and spacing. Re-enable copyright component in the footer. Add assets for project images.

---
+++ src/components/card.ts
@@ -0,0 +1,53 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { globalStyles } from "../styles/global";
+
+@customElement("card-component")
+class CardComponent extends LitElement {
+  @property({ type: String }) accessor image = "";
+  @property({ type: String }) accessor title = "";
+  @property({ type: String }) accessor content = "";
+
+  static styles = [
+    globalStyles,
+    css`
+      :host {
+        /* border: 1px solid var(--color-neutral-600); */
+        /* border-radius: 1rem; */
+        display: flex;
+        flex-direction: column;
+        /* gap: 1rem; */
+        /* padding: 1rem; */
+      }
+
+      #bottom {
+        background-color: var(--color-neutral-800);
+        border-radius: 0 0 1rem 1rem;
+        padding: 0.5rem 1rem;
+      }
+
+      #top {
+        background-color: var(--color-neutral-800);
+        border-radius: 1rem 1rem 0 0;
+        padding: 0.5rem 1rem;
+      }
+
+      img {
+        width: 100%;
+      }
+    `,
+  ];
+
+  render() {
+    return html`
+      <div id="top">
+        <h2>${this.title}</h2>
+      </div>
+      <div id="center"></div>
+      <img src="${this.image}" alt="${this.title}" />
+      <div id="bottom">
+        <slot name="content"></slot>
+      </div>
+    `;
+  }
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,12 @@
-import { css } from 'lit';
+import { css } from "lit";
 
-export const globalStyles = css``;
-
+export const globalStyles = css`
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
+`;
