186f84d0c68d0fa27a8d2756c0f8363d53ef0bbf
Andrew Noblet <andrewbnoblet@gmail.com>
2025-05-31 12:27:54 -0400

    feat(contact): add contact component with phone icon
    Integrate a new contact component featuring a phone icon for improved accessibility and user interaction. Updated styles for better visual consistency and added font preconnect links for performance optimization.

--- src/components/contact/index.ts
+++ src/components/contact/index.ts
@@ -1,31 +1,27 @@
+import "@components/button";
+import { calendarMonth } from "@icons/calendar-month";
+import { phone } from "@icons/phone";
 import { css, html } from "lit";
 import { customElement } from "lit/decorators.js";
-import { calendarMonth } from "../../icons/calendar-month";
-import { globalStyles } from "../../styles/global";
 import { Base } from "../base";
-import "../button";
 
 @customElement("contact-component")
 export class ContactComponent extends Base {
   static styles = [
-    globalStyles,
+    ...super.styles,
     css`
       :host {
+        background-color: var(--color-teal-200);
+        border-radius: 50%;
         bottom: 1rem;
+        box-shadow: var(--box-shadow);
+        color: var(--color-neutral-900);
+        outline: 1px solid #000;
         position: fixed;
         right: 1rem;
         width: max-content;
       }
 
-      a {
-        text-decoration: none;
-        color: inherit;
-      }
-
-      button-component {
-        transition: transform 0.2s var(--ease, ease);
-      }
-      
       a:hover button-component {
         transform: scale(1.04);
       }
@@ -34,10 +30,13 @@ export class ContactComponent extends Base {
 
   render() {
     return html`
-      <a href="${this.href}" target="_blank" rel="noopener noreferrer">
-        <button-component variant="primary">
-          <span>Schedule</span>${calendarMonth}
-        </button-component>
+      <a
+        class="padding"
+        href="${this.href}"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        ${phone}
       </a>
     `;
   }
