16ce08b4408e405ff0d0975487fc35f8ff86e347
Andrew Noblet <andrewbnoblet@gmail.com>
2025-06-01 14:57:47 -0400

    refactor(components): replace chip with tag component
    Replaced the chip component with the tag component across multiple files for consistency and improved functionality. Updated imports and adjusted usage in affected files. Removed chip.ts and added tag.ts to streamline the codebase.

---
+++ src/components/tag.ts
@@ -0,0 +1,30 @@
+import { Base } from "@components/base";
+import { css, html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("tag-component")
+class TagComponent extends Base {
+  static styles = [
+    ...super.styles,
+    css`
+      :host {
+        border: 1px solid var(--color-neutral-600);
+        border-radius: 0.25rem;
+        display: block;
+        font-size: 0.875rem;
+        padding: 0.125rem 0.5rem;
+      }
+
+      :host(:hover) {
+        background-color: var(--color-neutral-600);
+        cursor: pointer;
+        transition: background-color 0.1s ease-in-out;
+      }
+    `,
+  ];
+
+  render() {
+    return html`<slot></slot>`;
+  }
+}
+
